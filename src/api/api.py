93fa9cfdb5d50b0b096fbeddc92787cfc2005ed5
Cole-T-Harris <cole.t.harr@gmail.com>
2024-01-25 17:50:05 -0700

    Add api into routing directory

--- src/api/api.py
+++ src/api/api.py
@@ -3,6 +3,6 @@ from fastapi import APIRouter
 from src.api.endpoints import locations, products, health
 
 api_router = APIRouter()
-api_router.include_router(locations.router, prefix="/locations")
-api_router.include_router(products.router, prefix="/products")
+api_router.include_router(locations.router, prefix="/api/locations")
+api_router.include_router(products.router, prefix="/api/products")
 api_router.include_router(health.router, prefix="/health")
